6a4332be0ca3cf3396aeca5dfaa5e4cc7c9adae8
Austin Heiman <atheimanksu@gmail.com>
2014-11-20 09:48:19 -0600

    Update urls.py

--- conf/urls.py
+++ conf/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^app_name/', include('app_name.urls', namespace='app_name')),
+    #url(r'^app_name/', include('app_name.urls', namespace='app_name')),
     url(r'^admin/', include(admin.site.urls)),
 )
